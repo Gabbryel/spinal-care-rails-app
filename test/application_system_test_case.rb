ed868ba4d002f0593107783988048a47a143ce2d
Gabriel Ursache <gabriel@markov.ro>
2022-06-29 12:14:45 +0300

    upgraded to turborails

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,4 +5,5 @@ Webdrivers::Chromedriver.update
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   driven_by :headless_chrome
+  include TurboAssertionsHelper
 end
